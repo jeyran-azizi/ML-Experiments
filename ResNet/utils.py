f2afd4d1419ab24206fffa7e30e388bdc3120a0c
jeyran-azizi <jeyran.azizi1411@gmail.com>
2025-03-25 17:00:56 +0330

    Create utils.py

---
+++ ResNet/utils.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 + 0.5  # Unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
